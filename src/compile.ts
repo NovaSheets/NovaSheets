21447a410fe0285174ed9021fc19c8920d75ae34
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-07-03 15:10:39 +1200

    Make compile script properly async
    Use fs/promises instead of synchronous fs.

--- src/compile.ts
+++ src/compile.ts
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+import fs from 'fs/promises';
+import path from 'path';
 const isNode = typeof process !== 'undefined' && process?.versions?.node;
 const glob = isNode && require('glob');
 
@@ -7,40 +7,40 @@ import NovaSheets from './index';
 import parse from './parse';
 
 async function compileNovaSheets(source: string, outPath: string, novasheets: NovaSheets): Promise<void> {
-    outPath = outPath.replace(/[/\\]/g, path.sep);
+
     const compile = async (inputFiles: string[]): Promise<void> => {
         for (let input of inputFiles) {
-            await fs.readFile(input, 'utf8', async (err: Error, contents: string) => {
-                if (err) throw `FS_ReadError: Input file '${input}' not found.\n` + err.message;
+            const contents = await fs.readFile(input, { encoding: 'utf8' }).catch(err => {
+                throw `ReadError: Input file '${input}' not found.\n` + err.message;
+            });
 
-                let output = outPath;
+            let output = outPath.replace(/[/\\]/g, path.sep);
 
-                const folder: string = output.includes(path.sep) ? output.replace(/[/\\][^/\\]+$/, '') : '';
-                if (folder) {
-                    fs.mkdir(folder, { recursive: true }, (err: Error) => {
-                        if (err) throw `FS_MkDirError: Could not create directory '${folder}'.\n` + err.message;
-                    });
-                }
-
-                const filename: string = input.replace(/.+[/\\]([^/\\]+)$/, '$1'); // 'foo/bar.ext' -> 'bar.ext'
-
-                if (!output) {
-                    if (hasGlobs) output = input.replace(/[/\\][^/\\]+$/, path.sep); // 'foo.ext' -> 'foo/'
-                    else output = input.replace(/\.\w+$/, '.css'); // 'foo.ext' -> 'foo.css'
-                }
-
-                if (output.endsWith(path.sep)) output += filename; // 'foo.nvss bar/' -> 'bar/foo.nvss'
-                else if (hasGlobs) output += path.sep + filename; // '*.nvss bar' -> 'bar/$file.nvss'
-                else if (!output.match(/\.\w+$/)) output += '.css'; // 'foo.nvss bar' -> 'bar.css'
-                output = output.replace(/\.\w+$/, '.css'); // force .css extension
-
-                await fs.writeFile(output, parse(contents, novasheets), (err: Error) => {
-                    if (err) throw `FS_WriteError: Cannot write to file '${output}'.\n` + err.message;
-                    console['log'](`<NovaSheets> Wrote file '${input}' to '${output}'`);
+            const folder: string = output.includes(path.sep) ? output.replace(/[/\\][^/\\]+$/, '') : '';
+            if (folder) {
+                await fs.mkdir(folder, { recursive: true }).catch(err => {
+                    if (err) throw `MkDirError: Could not create directory '${folder}'.\n` + err.message;
                 });
+            }
+
+            const filename: string = input.replace(/.+[/\\]([^/\\]+)$/, '$1'); // 'foo/bar.ext' -> 'bar.ext'
+
+            if (!output) {
+                if (hasGlobs) output = input.replace(/[/\\][^/\\]+$/, path.sep); // 'foo.ext' -> 'foo/'
+                else output = input.replace(/\.\w+$/, '.css'); // 'foo.ext' -> 'foo.css'
+            }
+
+            if (output.endsWith(path.sep)) output += filename; // 'foo.nvss bar/' -> 'bar/foo.nvss'
+            else if (hasGlobs) output += path.sep + filename; // '*.nvss bar' -> 'bar/$file.nvss'
+            else if (!output.match(/\.\w+$/)) output += '.css'; // 'foo.nvss bar' -> 'bar.css'
+            output = output.replace(/\.\w+$/, '.css'); // force .css extension
+
+            await fs.writeFile(output, parse(contents, novasheets)).catch(err => {
+                if (err) throw `WriteError: Cannot write to file '${output}'.\n` + err.message;
+                console.log(`<NovaSheets> Wrote file '${input}' to '${output}'`);
             });
         }
-    };
+    }
 
     const hasGlobs: boolean = glob?.hasMagic(source);
     if (hasGlobs) {
--- test/test-compile.js
+++ test/test-compile.js
@@ -2,20 +2,25 @@ const fs = require('fs');
 const { execSync: exec } = require('child_process');
 const { compile } = require('../src/index');
 
-function test_compile() {
+let counter = 1;
+
+async function compileTest(input, output) {
+    await compile(input, output).catch(err => console.error(err));
+    console.log(`${counter++} Compiled ${input} to ${output}.`);
+}
+
+(async () => {
     exec('tsc');
     try { fs.rmdirSync('./bin/', { recursive: true, force: true }); } catch { }
     fs.mkdirSync('./bin/glob/', { recursive: true });
     fs.mkdirSync('./test/bin/', { recursive: true });
     // Per-file compilation
-    compile('test/example.nvss', 'bin/');
-    compile('test/example.nvss', 'bin/no-extension');
-    compile('test/example.nvss', 'bin/output-w-ext.css');
+    await compileTest('test/invalid.nvss', 'bin/');
+    await compileTest('test/example.nvss', 'bin/no-extension');
+    await compileTest('test/example.nvss', 'bin/output-w-ext.css');
     // Globbed compilation
     for (let i = 0; i < 3; i++) {
         fs.copyFileSync('test/example.nvss', `test/bin/example${i}.nvss`);
     }
-    compile('test/bin/*.nvss', 'bin/glob/');
-}
-
-test_compile();
+    await compileTest('test/bin/*.nvss', 'bin/glob/');
+})()
